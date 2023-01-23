98e7cbb1987011a9097f2bd5c6bae469f47ea7de
Erin Stephens <erebstephens@gmail.com>
2023-01-22 21:25:19 -0600

    card/form created but having a few issues

--- components/forms/MemberForm.js
+++ components/forms/MemberForm.js
@@ -1,7 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../../utils/context/authContext';
+import { createMember, getMembers, updateMember } from '../../api/memberData';
 
-export default function MemberForm() {
+const initialState = {
+  name: '',
+  image: '',
+  role: '',
+};
+
+export default function MemberForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialState);
+  // eslint-disable-next-line no-unused-vars
+  const [members, setMembers] = useState([]);
+  const router = useRouter();
+  const { user } = useAuth;
+
+  useEffect(() => {
+    getMembers(user.uid).then(setMembers);
+
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj, user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updateMember(formInput)
+        .then(() => router.push(`/member/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createMember(payload).then(() => {
+        router.push('/members');
+      });
+    }
+  };
+
   return (
-    <div>MemberForm</div>
+    <Form onSubmit={handleSubmit}>
+      <h2>{obj.firebaseKey ? 'Update' : 'Create'} Member</h2>
+
+      <FloatingLabel controlId="floatingInput1" label="Member Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter full name"
+          name="name"
+          value={formInput.name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingInput2" label="Member Image" className="mb-3">
+        <Form.Control
+          type="url"
+          placeholder="Enter an image url"
+          name="image"
+          value={formInput.image}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingInput3" label="Member Role" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter role"
+          name="role"
+          value={formInput.role}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Member</Button>
+    </Form>
   );
 }
+
+MemberForm.propTypes = {
+  obj: PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+    role: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+MemberForm.defaultProps = {
+  obj: initialState,
+};
--- pages/members.js
+++ pages/members.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { getMembers } from '../api/memberData';
 import { useAuth } from '../utils/context/authContext';
@@ -13,7 +14,7 @@ export default function ShowMembers() {
 
   useEffect(() => {
     getAllMembers();
-  });
+  }, []);
 
   return (
     <div>
